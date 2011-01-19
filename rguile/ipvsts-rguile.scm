ca38e4d262635f6be8aa950e1b908c49c9f025ed
Jan Friesse <jfriesse@redhat.com>
2011-01-19 17:27:40 +0100

    Support for serial remote guile

--- rguile/ipvsts-rguile.scm
+++ rguile/ipvsts-rguile.scm
@@ -21,7 +21,7 @@
 ;; Author: Jan Friesse <jfriesse@redhat.com>
 ;;
 
-(define (rguile-server port)
+(define (rguile-server-tcp port)
   (define (create-socket)
     (let ((s (socket PF_INET SOCK_STREAM 0)))
       (setsockopt s SOL_SOCKET SO_REUSEADDR 1)
@@ -61,22 +61,51 @@
         (lambda () (accept-connections s))
         (lambda () (close s)))))
 
+(define (rguile-server-serial file)
+    (define (process-connection s)
+      (catch #t
+        (lambda ()
+          (let ((res (eval (read s) (interaction-environment))))
+            (cond ((eq? res *unspecified*) (simple-format s "(ok)"))
+                  (#t (simple-format s "(ok ~S)" res)))))
+        (lambda (key . args)
+          (simple-format s "(error '~A)" key))))
+
+    (define (accept-connections s)
+      (while #t
+        (catch #t
+          (lambda ()
+            (dynamic-wind
+              (lambda () #t)
+              (lambda () (process-connection s))
+              (lambda () #t)))
+        (lambda (key . args)
+          #f))))
+
+    (let ((s (open-file file "r+0")))
+      (dynamic-wind
+        (lambda () #t)
+        (lambda () (accept-connections s))
+        (lambda () (close s)))))
+
 (define (usage)
-  (simple-format (current-error-port) "usage: ~A port\n" (car (command-line)))
+  (simple-format (current-error-port) "usage: ~A [port number | /dev/serial-port]\n" (car (command-line)))
   (exit 1))
 
 (define (sig-handler sig)
-  (exit 0))
+  (primitive-exit 0))
 
 (sigaction SIGINT sig-handler SA_RESTART)
 (sigaction SIGPIPE SIG_IGN)
 
 (cond ((eq? (cdr (command-line)) '()) (usage))
       ((eq? (cddr (command-line)) '())
-       (let ((port (string->number (cadr (command-line)))))
-         (if port
-             (rguile-server port)
-             (begin
-               (simple-format (current-error-port) "illegal number -- ~A\n" (cadr (command-line)))
-               (usage)))))
+       (cond ((string->number (cadr (command-line)))
+              (let ((port (string->number (cadr (command-line)))))
+                (rguile-server-tcp port)))
+             ((= (string-prefix-length (cadr (command-line)) "/dev/") (string-length "/dev/"))
+                (rguile-server-serial (cadr (command-line))))
+             (#t
+              (simple-format (current-error-port) "illegal number or /dev/... -- ~A\n" (cadr (command-line)))
+              (usage))))
       (#t (usage)))
