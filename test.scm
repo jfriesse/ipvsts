9fd4c21ae29550f5528b4e378ae01e746f435de1
Jan Friesse <jfriesse@redhat.com>
2011-03-15 13:18:33 +0100

    Support for edit service

--- test.scm
+++ test.scm
@@ -181,9 +181,53 @@
                            (string-append addr ":" port)))))
 
     (define (mod-rules)
-      (delete-service-from-rules ip6 type addr port))
+      (delete-service-from-rules ip6 type addr port)
+      #t)
 
     (ipvsts:check 'delete-service
+                  (run-ipvsadm-params)
+                  (mod-rules)
+                  (equal? (ipvslocal:rules-sort
+                           (ipvslocal:parse:net-ip_vs cl #f)))))
+
+  (define (edit-service ip6 type addr port scheduler timeout one-packet netmask)
+    (define (run-ipvsadm-params)
+      (run-ipvsadm "-E"
+                   (cond ((equal? type 't) "-t")
+                         ((equal? type 'u) "-u")
+                         ((equal? type 'f) "-f"))
+                   (if (equal? type 'f)
+                       (string-append addr
+                                      (if ip6 " -6 " ""))
+                       (if ip6 (string-append "[" addr "]:" port)
+                           (string-append addr ":" port)))
+                   (if scheduler (string-append "-s " scheduler) "")
+                   (if timeout (string-append "-p " timeout) "")
+                   (if one-packet "-O" "")
+                   (if netmask (simple-format #f "-M ~A" netmask) "")))
+
+    (define (mod-rules)
+      (let ((service (find-service ip6 type addr port)))
+        (set-car! (cdddr service)
+                  (if scheduler scheduler "wlc"))
+        (set-car! (cddddr service)
+                  (string-append
+                   (if one-packet "ops " "")
+                   (if timeout (simple-format #f "persistent ~A ~A"
+                                              (* (string->number timeout) 1000)
+                                              (if ip6
+                                                  (if netmask
+                                                      (ip4addr->hexstr
+                                                       (string-append netmask ".0.0.0"))
+                                                      (ip4addr->hexstr "128.0.0.0"))
+                                                  (if netmask
+                                                      (ip4addr->hexstr netmask)
+                                                      (ip4addr->hexstr "255.255.255.255"))))
+                       "")))
+        #t))
+
+
+    (ipvsts:check 'edit-service
                   (run-ipvsadm-params)
                   (mod-rules)
                   (equal? (ipvslocal:rules-sort
@@ -263,6 +307,44 @@
                   (delete-service #f 't ip42 port2)
                   (delete-service #f 'u ip4 port)))
 
+  (define (test-edit-service ip4 ip42 ip6 ip62 port port2 fw-mark fw-mark2 fw-mark3 fw-mark4)
+    (ipvsts:check 'test-edit-service
+                  (clear-rules)
+                  (add-service #f 't ip4 port #f #f #f #f)
+                  (add-service #f 'u ip4 port #f #f #f #f)
+                  (add-service #f 'f fw-mark #f #f #f #f #f)
+                  (add-service #t 't ip6 port #f #f #f #f)
+                  (add-service #t 'u ip6 port #f #f #f #f)
+                  (add-service #t 'f fw-mark2 #f #f #f #f #f)
+
+                  (edit-service #f 't ip4 port "rr" #f #f #f)
+                  (edit-service #f 'u ip4 port "rr" #f #f #f)
+                  (edit-service #f 'f fw-mark #f "rr" #f #f #f)
+                  (edit-service #t 't ip6 port "rr" #f #f #f)
+                  (edit-service #t 'u ip6 port "rr" #f #f #f)
+                  (edit-service #t 'f fw-mark2 #f "rr" #f #f #f)
+
+                  (edit-service #f 't ip4 port #f "12" #f #f)
+                  (edit-service #f 'u ip4 port #f "12" #f #f)
+                  (edit-service #f 'f fw-mark #f #f "12" #f #f)
+                  (edit-service #t 't ip6 port #f "14" #f #f)
+                  (edit-service #t 'u ip6 port "wrr" "15" #f #f)
+                  (edit-service #t 'f fw-mark2 #f "wrr" "16" #f #f)
+
+                  (edit-service #f 't ip4 port #f "15" #f "255.255.255.0")
+                  (edit-service #f 'u ip4 port #f "16" #t "255.255.255.0")
+                  (edit-service #f 'f fw-mark #f #f "17" #f "255.255.0.0")
+                  (edit-service #t 't ip6 port #f "14" #f "128")
+                  (edit-service #t 'u ip6 port "wrr" "15" #t "127")
+                  (edit-service #t 'f fw-mark2 #f "wrr" "16" #f "126")
+
+                  (delete-service #f 't ip4 port)
+                  (delete-service #f 'u ip4 port)
+                  (delete-service #f 'f fw-mark #f)
+                  (delete-service #t 't ip6 port)
+                  (delete-service #t 'u ip6 port)
+                  (delete-service #t 'f fw-mark2 #f)))
+
   ;;    (write 'loaded)
   ;;    (write (ipvslocal:rules-sort
   ;;              (ipvslocal:parse:net-ip_vs cl #f)))
@@ -286,9 +368,11 @@
         (fw-mark4 "901"))
     (ipvsts:check 'rules
                   (test-add-service ip4 ip42 ip6 ip62 port port2
-                                    fw-mark fw-mark2 fw-mark3 fw-mark4))
+                                    fw-mark fw-mark2 fw-mark3 fw-mark4)
                   (test-del-service ip4 ip42 ip6 ip62 port port2
-                                    fw-mark fw-mark2 fw-mark3 fw-mark4)))
+                                    fw-mark fw-mark2 fw-mark3 fw-mark4)
+                  (test-edit-service ip4 ip42 ip6 ip62 port port2
+                                    fw-mark fw-mark2 fw-mark3 fw-mark4))))
 
 (test:ipvslocal:rules (rguile-client "127.0.0.1" (+ (cfg 'test:vm:rguile-port-base) 1)) 1 1)
 
